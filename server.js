6810b9ec46c24200a97591f64bb286d844aef59d
Dashavanth K <dashwanthk@gmail.com>
2019-07-24 20:00:20 +0530

    angular module addition

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var app  = express()
+
+app.get('/',(req,res)=>{
+    res.send("Hi There!")
+})
+app.listen(3000)
+console.log('express app is running on localhost:3000')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
